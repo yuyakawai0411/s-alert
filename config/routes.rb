ca5ad901a600d37a2d97e3e6c076618ffb8f00b8
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-06-18 17:31:49 +0900

    検索欄の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root to: "cards#index"
   resources :cards do
     resources :records, only: [:index, :create, :destroy]
+    collection do
+      get 'search'
+    end
   end
+
 end
