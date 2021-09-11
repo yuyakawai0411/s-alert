970061a4171f116133d7161081c5e71d58c30ce6
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-09-11 14:33:14 +0900

    Rails+MySQLをローカル(development)で動かす(ヘルスチェック失敗)

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ruby:2.6.5
 
-ENV RAILS_ENV="production"
+ENV RAILS_ENV="development"
 
 #nodejsのインストール
 RUN curl -fsSL https://deb.nodesource.com/setup_14.x | bash - \
@@ -25,10 +25,11 @@ RUN bundle install
 
 COPY . /app
 
+HEALTHCHECK --interval=30s --timeout=5s --start-period=240s --retries=5 CMD curl -f http://localhost:3000/ || exit 1
+
 # cronの実行
 # RUN bundle exec whenever --update-crontab 
 # CMD ["cron", "-f"] 
-
 
 #本番環境で行う動作
 COPY start.sh /start.sh
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ "${RAILS_ENV}" = "production" ]
 then
-    bundle exec rails assets:precompile RAILS_ENV=production
+    bundle exec rails assets:precompile 
 fi
 
 DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:drop
@@ -10,4 +10,4 @@ bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
 
-RAILS_SERVE_STATIC_FILES=1 bundle exec unicorn_rails -c /app/config/unicorn.rb -E production -p 3000
+# bundle exec unicorn_rails -c /app/config/unicorn.rb -E production -p 3000
