caa695c0f42f00524ae3141681c7e9724a4bbf6b
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-09-14 16:48:33 +0900

    Rails+MySQL+Unicorn+Nginxをローカル(production)で動かす(成功)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = "ws://52.197.100.124/cable" 
+  config.action_cable.url = "ws://localhost/cable" 
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
     # Action mailer gmail 
