47fb77962cfb7d4d5d34a80f5f43444f0961398e
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-09-16 12:28:33 +0900

    DNS名の変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   ActionCable.server.config.disable_request_forgery_protection = true
-  config.action_cable.url = "ws://ECS-sample-320993441.ap-northeast-1.elb.amazonaws.com/cable" 
+  config.action_cable.url = "ws://yuya5732.com/cable" 
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
     # Action mailer gmail 
