baca4a982dea8ac2d18d2566c1744f6354b5baae
yuyakawai0411 <84003054+yuyakawai0411@users.noreply.github.com>
2021-10-08 15:29:07 +0900

    biorhythmの計算を別クラスで定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,22 +51,7 @@ class UsersController < ApplicationController
   end
 
   def set_biorhythm
-    # 登録された誕生日を元に、現在から前後2週間のバイオリズムグラフを作成
-    pi = Math::PI
-    birth_day = @user.birth_day
-    today = Date.today  
-    delta = today - birth_day
-    @biorhythm_graph= {}
-    i = -15
-
-    30.times do 
-      key = today.next_day(i)
-      value = Math.sin(2 * pi * (delta + i) / 28)
-      @biorhythm_graph.store( "#{key}", value )
-      i = i + 1
-    end
-    # バイオリズムグラフの中から不安定日(0に近い値)を2つ算出
-    @unstable = @biorhythm_graph.min_by(2){|x,v| (v - 0).abs}
+    @theoretical_biortythm_graph = DrawBiortythmGraph.theoretical_biortythm(@user.birth_day, -15, 0)
+    @unstable_day = CalculateMinMax.calculate_min(@theoretical_biortythm_graph)
   end
-
 end
